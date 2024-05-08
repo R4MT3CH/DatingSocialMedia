0c2c851d267c6741e8ba70ee964a1d73fe6eae06
Ramin Roufeh <raminroufeh@gmail.com>
2024-05-07 23:14:05 -0700

    middleware update

--- middleware.js
+++ middleware.js
@@ -1,8 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ['/'],
-  ignoredRoutes: ['/api/webhook'],
+  publicRoutes: ['/','/api/webhook'],
 });
 
 export const config = {
